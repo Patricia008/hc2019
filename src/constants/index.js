48c515c9927ad3478f9d9a1f7efd8289ab2cfe67
Florin Barbuceanu <florin.barbuceanu@sap.com>
2019-02-28 20:24:34 +0200

    Initial parsing and input data

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  files: {
+    a: './data/a_example.txt',
+    b: './data/b_lovely_landscapes.txt',
+    c: './data/c_memorable_moments.txt',
+    d: './data/d_pet_pictures.txt',
+    e: './data/e_shiny_selfies.txt'
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-const main = () => console.log('I am a tiny program that will win!');
+const fs = require('fs');
+const { files } = require('./constants');
 
-main();
+const parseFile = filename => {
+  const fileContent = fs
+    .readFileSync(filename)
+    .toString()
+    .split('\n');
+
+  const size = parseInt(fileContent.shift());
+  return fileContent
+    .map((slideStr, slideIndex) => parseSlide(slideIndex, slideStr))
+    .reduce(
+      (agg, currentSlide) => {
+        if (currentSlide.type === 'H') {
+          agg.horizontal.push(currentSlide);
+        } else if (currentSlide.type === 'V') {
+          agg.vertical.push(currentSlide);
+        }
+        return agg;
+      },
+      { horizontal: [], vertical: [] }
+    );
+};
+
+const parseSlide = (slideId, slideStr) => {
+  const [type, _, ...tags] = slideStr.split(/\s+/);
+  return { type, id: slideId, tags: new Set(tags) };
+};
+
+const parsedSlides = parseFile(files.d);
+
+console.log(parsedSlides);
