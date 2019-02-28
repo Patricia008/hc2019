56b60bbf946eecb80ee9477be2fe1a4584f3817c
Krody Octavian Robert <octavian.krody@sap.com>
2019-02-28 20:34:48 +0200

    Scoring

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-const fs = require('fs');
-const { files } = require('./constants');
+const fs = require("fs");
+const { files } = require("./constants");
+
+const union = (a, b) => new Set([...a, ...b]);
+const intersection = (a, b) => new Set([...a].filter(x => b.has(x)));
+const difference = (a, b) => new Set([...a].filter(x => !b.has(x)));
+
+const score = (current, next) => {
+  const same = intersection(current.tags, next.tags);
+  const differenceA = difference(current.tags, next.tags);
+  const differenceB = difference(next.tags, current.tags);
+
+  return Math.min(same, differenceA, differenceB);
+};
 
 const parseFile = filename => {
   const fileContent = fs
     .readFileSync(filename)
     .toString()
-    .split('\n');
+    .split("\n");
 
-  const size = parseInt(fileContent.shift());
   return fileContent
     .map((slideStr, slideIndex) => parseSlide(slideIndex, slideStr))
     .reduce(
       (agg, currentSlide) => {
-        if (currentSlide.type === 'H') {
+        if (currentSlide.type === "H") {
           agg.horizontal.push(currentSlide);
-        } else if (currentSlide.type === 'V') {
+        } else if (currentSlide.type === "V") {
           agg.vertical.push(currentSlide);
         }
         return agg;
